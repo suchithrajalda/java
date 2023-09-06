e6484cf00ee6862762ebefcef7b80daf373ab27a
suchithrajalda <95867695+suchithrajalda@users.noreply.github.com>
2023-09-06 16:25:44 +0530

    Update First.java

--- First.java
+++ First.java
@@ -2,6 +2,6 @@ class First
 {
 	public static void main (String args[])
 	{
-		System.out.println("hello world!! This is suchithra welcome to java learning");
+		System.out.println("hello world!! This is suchithra welcome to coding hub innovations");
 	}
 }
